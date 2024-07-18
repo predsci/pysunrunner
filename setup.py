d8b090205046224127a7cbe1ca8ebe4ca4c80425
Pete Riley <pete@predsci.com>
2024-07-18 13:01:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pysunrunner",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(),
     install_requires=[
         'numpy==2.0.0',
