b105880dff98744437106c25aabf30b011f6e85d
BadOmen59 <137584350+BadOmen59@users.noreply.github.com>
2024-07-18 15:27:03 -0700

    syntax error setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pysunrunner",
-    version="4.5.0,
+    version="4.5.0",
     packages=find_packages(),
     install_requires=[
         'numpy==2.0.0',
